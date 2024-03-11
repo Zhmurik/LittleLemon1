e8b1ec62ee7e4b708d35d4aea3789d7db8a96e4e
zhmurik <azhmurkova@gmail.com>
2024-03-11 16:35:45 -0700

    add rest_framework create serializers.py add views.py  add path urls.py project level  add path urls.py app level

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,4 +1,24 @@
 from django.shortcuts import render
+from rest_framework import generics, viewsets
+
+
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer
+
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class SinglMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
 
 
 def index(request):
