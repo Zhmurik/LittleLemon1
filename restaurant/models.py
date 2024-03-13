12f5c371baf60c28ec1c6ebd902d1f33b23a2da5
zhmurik <azhmurkova@gmail.com>
2024-03-13 16:38:08 -0700

    add __str__(self) models.py add authenticated method djoser

--- restaurant/models.py
+++ restaurant/models.py
@@ -4,10 +4,16 @@ from django.db import models
 class Menu(models.Model):
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    inventory = models.IntegerField(max_length=5)
+    inventory = models.SmallIntegerField(max_length=5)
+
+    def __str__(self):
+        return self.title
+
+    def get_item(self):
+        return f'{self.title} : {str(self.price)}'
 
 
 class Booking(models.Model):
     name = models.CharField(max_length=255)
-    no_of_guests = models.IntegerField(max_length=6)
+    no_of_guests = models.SmallIntegerField(max_length=6)
     booking_date = models.DateTimeField()
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render
 from rest_framework import generics, viewsets
-
+from rest_framework.permissions import IsAuthenticated
 
 from .models import Menu, Booking
 from .serializers import MenuSerializer, BookingSerializer
 
 
 class MenuItemsView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
 
 class SinglMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
